3f50e46436c1929d24f1baba07e4484dc8fd9300
Niharika Mahadevappa <niharikamahadevappa0@gmail.com>
2025-12-07 15:13:15 +0530

    Add files via upload

--- Phase 0/Step2/Task 4/pitch.py
+++ Phase 0/Step2/Task 4/pitch.py
@@ -1,1 +1,11 @@
+import librosa
+import numpy as np
 
+audio_path = "Recording.wav"
+
+y, sr = librosa.load(audio_path)
+
+f0 = librosa.yin(y, fmin=80, fmax=400)
+pitch = np.nanmean(f0)
+
+print("Pitch (Hz):", pitch)
