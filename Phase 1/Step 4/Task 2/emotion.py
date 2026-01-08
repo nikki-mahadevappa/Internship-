86af18dd73d9885b1e148312873e6f8083193011
Niharika Mahadevappa <niharikamahadevappa0@gmail.com>
2026-01-08 15:40:04 +0530

    Add files via upload

--- Phase 1/Step 4/Task 2/emotion.py
+++ Phase 1/Step 4/Task 2/emotion.py
@@ -1,1 +1,11 @@
+from deepface import DeepFace
 
+result = DeepFace.analyze(
+    img_path="face.jpeg",
+    actions=["emotion"],
+    enforce_detection=False
+)
+
+emotion = result[0]["dominant_emotion"]
+
+print(f"The subject appears to be feeling: {emotion}.")
