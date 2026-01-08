a04096ac0f82f8c8b29ad15b6bb3a0d10c34775d
Niharika Mahadevappa <niharikamahadevappa0@gmail.com>
2026-01-08 14:35:03 +0530

    Add files via upload

--- Phase 1/Step 4/Task 1/transcribe.py
+++ Phase 1/Step 4/Task 1/transcribe.py
@@ -1,1 +1,10 @@
+import whisper
+import os
 
+os.environ["PATH"] += os.pathsep + r"C:\Users\Lenovo\OneDrive\Desktop\ffmpeg-8.0.1-essentials_build\ffmpeg-8.0.1-essentials_build\bin"
+
+model = whisper.load_model("base")
+
+result = model.transcribe("melody.mp3")
+
+print(result["text"])
