83c604fa27c52c292fd4b7b9197ddabf8e8639ec
Niharika Mahadevappa <niharikamahadevappa0@gmail.com>
2026-03-11 11:28:37 +0530

    Refactor API key management in haiku.py
    Updated API key handling to use environment variable.

--- Phase 1/Step 5/Task 1/haiku-bot/haiku.py
+++ Phase 1/Step 5/Task 1/haiku-bot/haiku.py
@@ -1,7 +1,8 @@
 from google import genai
 
 # Create Gemini client with your API key
-client = genai.Client(api_key="AIzaSyC********")
+import os
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def generate_haiku(word):
     prompt = f"""
@@ -21,4 +22,5 @@ def generate_haiku(word):
 word = input("Enter a word: ")
 print("\nYour Haiku:\n")
 print(generate_haiku(word))
+
 
